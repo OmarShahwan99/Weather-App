6cca4fb29021ea17ad7923c173d5341bcde3a23e
AliAlshikh9099 <119836658+AliAlshikh9099@users.noreply.github.com>
2023-06-15 20:29:55 +0300

    s

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -4,10 +4,14 @@ import weatherContext from "../../store/weather-context";
 import { useTranslation } from "react-i18next";
 
 const Logo = () => {
-  const [t] = useTranslation("global") 
+  const [t] = useTranslation("global");
   const { tempUnit } = useContext(weatherContext);
   return (
-    <h1 className="text-3xl text-primary col-span-1">{t('logo')}&deg;{tempUnit}</h1>
+    <h1 className="text-3xl text-primary col-span-1">
+      <a href="#s">
+        {t("logo")}&deg;{tempUnit}
+      </a>
+    </h1>
   );
 };
 
